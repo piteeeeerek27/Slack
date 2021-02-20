aa78210a1eee8085e70f76ab7d75b2457166544f
Piotr Rześny <rzesnypPiotrr@gmail.com>
2021-02-20 08:22:47 +0100

    start

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+	apiKey: "AIzaSyBs6ctgOJ2n2_DTI5P49vaSlefoL1GvdS8",
+	authDomain: "slack-clone-48c26.firebaseapp.com",
+	projectId: "slack-clone-48c26",
+	storageBucket: "slack-clone-48c26.appspot.com",
+	messagingSenderId: "620844053063",
+	appId: "1:620844053063:web:c548813c5925ccf2cfb762",
+	measurementId: "G-64EW0NSYD9",
+};
